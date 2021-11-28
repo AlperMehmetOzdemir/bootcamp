71c57eddb746bcc89959b9a9a91beab7b2dc51ce
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-11-28 13:31:25 +0300

    Added produts app and product model

---
+++ ecommerce/products/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+from products import enums
+
+
+class Product(models.Model):
+    sku = models.CharField(verbose_name=_("SKU"), max_length=100, unique=True)
+    name = models.CharField(max_length=255, verbose_name=_("Name"))
+    description = models.TextField(max_length=2000, verbose_name=_("Description"))
+    color = models.CharField(choices=enums.Colors.choices, verbose_name=_("Color"), max_length=20)
+    size = models.CharField(max_length=30, verbose_name=_("Size"))
+
+    class Meta:
+        verbose_name = _("product")
+        verbose_name_plural = _("products")
+
+    def __str__(self):
+        return self.sku
